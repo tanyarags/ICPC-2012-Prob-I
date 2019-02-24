47d288b5649b6f6e3730da432d1250c312e0d3c0
tanya <tanyarags@gmail.com>
2019-02-24 22:57:32 +0530

    initial commit

---
+++ rapyuta/rapyuta/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rapyuta
+//
+//  Created by tanyarags on 24/02/19.
+//  Copyright © 2019 tanyarags. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
